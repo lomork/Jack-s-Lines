e2c41f200906308b4a406177f0b6f691f797cefd
lomork <amsingh2001@gmail.com>
2025-12-27 22:11:07 -0500

    7th commit

--- lib/screens/game/play_friends.dart
+++
@@ -1,8 +0,0 @@
-import '../../database/online_service.dart';
-import 'play_online.dart';
-
-class PlayFriendsHandler extends PlayOnlineHandler {
-  PlayFriendsHandler(super.service, super.onExternalMove);
-
-// Future: Add private room logic or friend-specific rematches
-}
